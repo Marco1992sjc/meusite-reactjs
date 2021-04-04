9fc4075c398fb92d295dfa81f4d662350d0f5bfe
marco1992sjc <marco1992sjc@gmail.com>
2021-04-04 14:01:14 -0300

    Seo
    ajustando o  Seo da página e implementando  com o plugin next-Seo para o Nextjs.
    Estou estudando sobre o assunto também

--- pages/curriculo.js
+++ pages/curriculo.js
@@ -1,7 +1,7 @@
 import React from "react";
 import NavBar from './components/NavBar';
 import Meucurriculo from './components/Meucurriculo';
-
+import { NextSeo } from 'next-seo';
 
 
 
@@ -13,8 +13,36 @@ import Meucurriculo from './components/Meucurriculo';
 function Curriculo() {
     return (
       <>
-
-
+<NextSeo
+      title="Marco Aurélio Desenvolvedor ReactJs e React Native"
+      description="Marco Aurélio Desenvolvedor front-end e mobile, Reactjs,React Native,Nextjs  de São José dos Campos"
+      canonical="https://marcodev.vercel.app/"
+      openGraph={{
+        url: 'https://marcodev.vercel.app/curriculo',
+        title: 'Marco Aurélio de Moraes Oliveira',
+        description: 'Desenvolvedor de São José dos Campos',
+        images: [
+          {
+            url: 'https://marcodev.vercel.app/marco.jpg',
+            width: 800,
+            height: 600,
+            alt: 'Marco Aurélio desenvolvedor mobile, frontend, experiéncia como vigilante',
+          },
+          {
+            url: 'https://marcodev.vercel.app/marco.jpg',
+            width: 900,
+            height: 800,
+            alt: 'Marco Auréio desenvolvedor de São José dos Campos',
+          },
+          { url: 'https://marcodev.vercel.app/marco.jpg' },
+          { url: 'https://marcodev.vercel.app/marco.jpg' },
+        ],
+        site_name: 'marcodev',
+      }}
+      facebook={{
+        appId: '100003823449139',
+      }}
+/>
      
       <NavBar />
 <div className="Meucurriculo">
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,46 @@
 import React from "react";
 import NavBar from './components/NavBar';
-
+import { NextSeo } from 'next-seo';
 
 
 
   export default  function App(){
     return(
 <>
+
+
+<NextSeo
+      title="Marco Aurélio Desenvolvedor"
+      description="Marco Aurélio Desenvolvedor front-end e mobile de São José dos Campos"
+      canonical="https://marcodev.vercel.app/"
+      openGraph={{
+        url: 'https://marcodev.vercel.app/curriculo',
+        title: 'Marco Aurélio de Moraes Oliveira',
+        description: 'Desenvolvedor de São José dos Campos',
+        images: [
+          {
+            url: 'https://marcodev.vercel.app/marco.jpg',
+            width: 800,
+            height: 600,
+            alt: 'Marco Aurélio desenvolvedor mobile, frontend, experiéncia como vigilante',
+          },
+          {
+            url: 'https://marcodev.vercel.app/marco.jpg',
+            width: 900,
+            height: 800,
+            alt: 'Marco Auréio desenvolvedor de São José dos Campos',
+          },
+          { url: 'https://marcodev.vercel.app/marco.jpg' },
+          { url: 'https://marcodev.vercel.app/marco.jpg' },
+        ],
+        site_name: 'marcodev',
+      }}
+      facebook={{
+        appId: '100003823449139',
+      }}
+    />
+
+
 <NavBar />
 </>
 
