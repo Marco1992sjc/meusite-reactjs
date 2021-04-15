8e4af89b7716979e0b45511abe56b9195caace2d
Marco1992sjc <marco1992sjc@gmail.com>
2021-04-15 18:15:08 -0300

    atualizando
    colocando conteúdo do portfolio na página.

--- pages/components/Meuportfolio.js
+++ pages/components/Meuportfolio.js
@@ -1,3 +1,4 @@
+
 import Card from 'react-bootstrap/Card'
 
 
@@ -5,6 +6,12 @@ import Card from 'react-bootstrap/Card'
 
 function Meuportfolio(){
   return(
+
+
+
+
+<>
+
 
 <Card className="card" style={{ width: '20rem' }} bg="light">
 <a href="./reactnews.png" >
@@ -20,6 +27,23 @@ function Meuportfolio(){
 </Card>
 
 
+
+<Card className="card2" style={{ width: '20rem' }} bg="light">
+
+  <Card.Body>
+  <iframe width="270" height="270" src="https://www.youtube.com/embed/wtlzPD5VS0g" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <Card.Title><h1>YoutubeMusic Desktop</h1></Card.Title>
+    <Card.Text>
+      <a href="https://drive.google.com/file/d/1Wjcpi547JG6zU_LO-tq41VooDGsI_PeS/view?usp=sharing"><u>(Download)</u></a> Player do youtube music desenvolvido no framework electronjs para desktop, aplicativo bem símples que ao tempo irei atualizando conforme aprendo no framework
+      (inlusive se tornou meu player padrão rs, devido a google não disponibilizar aplicativo para windows)
+    </Card.Text>
+
+  </Card.Body>
+</Card>
+
+
+
+</>
 
   )
 }
