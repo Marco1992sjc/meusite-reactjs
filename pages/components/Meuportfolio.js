c87a0d8b1b4c060600a9b4fa2e298778de6b24d5
Marco1992sjc <marco1992sjc@gmail.com>
2021-04-16 02:46:33 -0300

    updates
    link atualizado

--- pages/components/Meuportfolio.js
+++ pages/components/Meuportfolio.js
@@ -34,7 +34,7 @@ function Meuportfolio(){
   <iframe width="270" height="270" src="https://www.youtube.com/embed/wtlzPD5VS0g" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     <Card.Title><h1>YoutubeMusic Desktop</h1></Card.Title>
     <Card.Text>
-      <a href="https://drive.google.com/file/d/1Wjcpi547JG6zU_LO-tq41VooDGsI_PeS/view?usp=sharing"><u>(Download)</u></a> Player do youtube music desenvolvido no framework electronjs para desktop, aplicativo bem símples que ao tempo irei atualizando conforme aprendo no framework
+      <a href="https://drive.google.com/file/d/1R7ksiLRXDmfeS4jZCFLyrVYSgMxSJvx8/view?usp=sharing"><u>(Download)</u></a> Player do youtube music desenvolvido no framework electronjs para desktop, aplicativo bem símples que ao tempo irei atualizando conforme aprendo no framework
       (inlusive se tornou meu player padrão rs, devido a google não disponibilizar aplicativo para windows)
     </Card.Text>
 
